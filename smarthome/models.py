c24fa4ac431998c5b6af546ddc33141ffbed4722
mmHare <madmarchhare65@gmail.com>
2025-10-14 23:38:03 +0200

    index + detail html

--- smarthome/models.py
+++ smarthome/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class HomeUser(models.Model):
-    user_name = models.CharField("name", max_length=200)
+    user_name = models.CharField("name", unique=True, max_length=200)
+    user_password = models.CharField("password", max_length=50)
 
     def __str__(self):
         return self.user_name
--- smarthome/views.py
+++ smarthome/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import HomeUser
 
 
-def index(request):
-    return HttpResponse("Yo, this is SmartHome index.")
+# def index(request):
+#     return HttpResponse("Yo, this is SmartHome index.")
+
+
+class IndexView(generic.ListView):
+    template_name = "smarthome/index.html"
+    context_object_name = "home_users_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return HomeUser.objects.order_by("-user_name")
+
+
+class DetailView(generic.DetailView):
+    model = HomeUser
+    template_name = "smarthome/detail.html"
