635ffa873813f86387ab7d4eb225e87b9cdc3b55
Biorth <zajacmaciej01@gmail.com>
2025-11-20 11:26:47 +0100

    podświetlenie linku room list

--- smarthome/static/js/navbar_active.js
+++ smarthome/static/js/navbar_active.js
@@ -1,8 +1,15 @@
 document.querySelectorAll('.navBar li a').forEach(
     link => {
         // if(link.href === window.location.href) {
-        if(window.location.href.includes(link.href)) {
-            link.setAttribute('aria-current', 'page')
+        if(window.location.href.includes("rooms")) {
+            if(link.href.includes("rooms")) {
+                link.setAttribute('aria-current', 'page')
+            }
+        }
+        else if(!window.location.href.includes("rooms")) {
+            if(window.location.href.includes(link.href)) {
+                link.setAttribute('aria-current', 'page')
+            }
         }
     }
 )
